7ce79868726f7a0623d635f84031d042330339b6
Kasey <kasey_crocker@yahoo.com>
2022-08-07 23:20:38 -0400

    updated template

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
-//
-//const pageHTML = generatePage(name, github);
-//
-//fs.writeFile('./index.html', pageHTML, err => {
-//    if (err) throw err;
-//  
-//    console.log('Portfolio complete! Check out index.html to see the output!');
-//  });
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -142,6 +134,12 @@ if (!portfolioData.projects) {
 
 promptUser()
   .then(promptProject)
-  .then(projectData => {
-    console.log(projectData)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+    
+    fs.writeFile('./index.html', pageHTML, err => {
+        if (err) throw new Error(err);
+      
+        console.log('Page created! Check out index.html in this directory to see it!');
+      });
   });
